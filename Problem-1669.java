83c13424b4835e21eefec66c4ecbc30acede7bad
Sarang Shivaji Patil <107185585+SarangPatil9700@users.noreply.github.com>
2025-01-01 17:16:18 +0530

    Update Problem-1669.java

--- Problem-1669.java
+++ Problem-1669.java
@@ -1,4 +1,4 @@
-/**1669. Merge In Between Linked Lists
+/**
  * Definition for singly-linked list.
  * public class ListNode {
  *     int val;
@@ -7,7 +7,10 @@
  *     ListNode(int val) { this.val = val; }
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
- */
+ */1669. Merge In Between Linked Lists You are given two linked lists: list1 and list2 of sizes n and m respectively.
+Remove list1's nodes from the ath node to the bth node, and put list2 in their place.
+The blue edges and nodes in the following figure indicate the result:
+ 
 class Solution {
     public ListNode mergeInBetween(ListNode list1, int a, int b, ListNode list2) {
         ListNode start = null;
